e209e5ad9638364da72d49ddf209bc21896242dd
Varchasva <tiwarivarchasva@yahoo.in>
2021-01-23 14:38:39 +0530

    Added lombok for cart entity

--- src/main/java/com/hackerearth/cafeteriaapp/entity/Cart.java
+++ src/main/java/com/hackerearth/cafeteriaapp/entity/Cart.java
@@ -1,10 +1,21 @@
 package com.hackerearth.cafeteriaapp.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
+@Table(name = "cart")
 public class Cart {
     @Id
     @Column(name = "id")
